7f5da474574e3e603c544b3d4f7cf3c12970c53b
Bethmi Jayamila <bethmij@gmail.com>
2024-03-14 00:16:11 +0530

    add ItemController

---
+++ mapping-spec/src/main/java/lk/ijse/gdse66/api/ItemController.java
@@ -0,0 +1,43 @@
+package lk.ijse.gdse66.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String SaveItemJson(){
+        return "SaveItemJson(): I accept only application/Json";
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    public String SaveItemXml(){
+        return "SaveItemXml(): I accept only application/xml";
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String SaveItemMultipartFormData(){
+        return "SaveItemMultipartFormData(): I accept only multipart/form-data";
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String SaveItemUrlEncoded(){
+        return "SaveItemUrlEncoded(): I accept only application/x-www-form-urlencoded";
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String GetItemAsJson(){
+        return "GetItemAsJson()";
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String GetItemASXml(){
+        return "GetItemsAsXml()";
+    }
+
+}
