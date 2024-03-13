0f8476b432a92e3ef30cb6508cdc2317a854ea33
Bethmi Jayamila <bethmij@gmail.com>
2024-03-13 08:16:07 +0530

    add MappingSpecController

--- mapping-spec/src/main/java/lk/ijse/gdse66/api/MappingSpecController.java
+++ mapping-spec/src/main/java/lk/ijse/gdse66/api/MappingSpecController.java
@@ -1,4 +1,21 @@
 package lk.ijse.gdse66.api;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/one")
 public class MappingSpecController {
+    public MappingSpecController() {
+        System.out.println("MappingSpecController - constructor");
+    }
+
+    @GetMapping
+    public String getMethod(){
+        return "getMethod()";
+    }
+
+
 }
