c88b2444f249cfe7ae994dadeed83aa8c7c345e5
Bethmi Jayamila <bethmij@gmail.com>
2024-03-13 23:42:13 +0530

    add query params

--- mapping-spec/src/main/java/lk/ijse/gdse66/api/CustomerController.java
+++ mapping-spec/src/main/java/lk/ijse/gdse66/api/CustomerController.java
@@ -1,25 +1,23 @@
 package lk.ijse.gdse66.api;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @GetMapping
+/*    @GetMapping
     public String getMethod(){
         return "Received GET request";
-    }
+    }*/
 
     /*---------- path variables -----------*/
-//    @GetMapping("/{city}")
-//    public String getCustomerByCity(@PathVariable("city") String city){
-//        return String.format("getCustomerByCity(String city): %s", city);
-//    }
+
+/*    @GetMapping("/{city}")
+    public String getCustomerByCity(@PathVariable("city") String city){
+        return String.format("getCustomerByCity(String city): %s", city);
+    }*/
 
     @GetMapping("/{city}/{name}")
     public String getCustomerByCityName(@PathVariable("city") String cusCity, @PathVariable("name") String cusName){
@@ -29,6 +27,44 @@ public class CustomerController {
     @GetMapping("/{id:C00-\\d{3}}")
     public String getCustomerByID(@PathVariable("id") String cusID){
         return String.format("getCustomerByID (String cusID) : %s",cusID);
+    }
+
+   /*------------ query parameter ---------------*/
+
+    /*@GetMapping
+    public String getMethodWithParam1(String id){
+        return String.format("getMethodWithParam1(String id) : %s", id);
+    }*/
+
+    @GetMapping(params = "id")
+    public String getMethodWithParam1(@RequestParam(value = "id") String id){
+        return String.format("getMethodWithParam1(String id) : %s", id);
+    }
+
+    @GetMapping
+    public String getMethodWithParam2(@RequestParam(value = "CusAge", required = false) String age){
+        return String.format("getMethodWithParam2(String age): %s",age);
+    }
+
+    @GetMapping(params = {"id", "name", "age"})
+    public String getMethodWithParam3(@RequestParam("id") String cusId, @RequestParam("name") String name, @RequestParam("age") int age){
+        return String.format(
+                "getMethodWithParam3(String id, String name, int age): %s, %s, %s"
+                ,cusId,name,age);
+    }
+
+    /* ---------- application/x-www-form-urlencoded OR query parameters ---------    */
+
+    /*@PostMapping
+    public String SaveCustomer(String id, String name, int age){
+        return String.format(
+                "SaveCustomer(String id, String name, int age): %s, %s, %s",
+                id,name,age);
+    }*/
+
+    @PostMapping
+    public String SaveCustomer(@ModelAttribute CustomerDTO customer){
+        return customer.toString();
     }
 
 }
