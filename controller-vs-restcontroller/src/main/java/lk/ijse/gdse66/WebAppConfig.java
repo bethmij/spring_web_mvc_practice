a1a889a9bca85d531beef43b8138bb2abbd44b71
Bethmi Jayamila <bethmij@gmail.com>
2024-03-13 07:48:35 +0530

    config WebAppConfig

--- controller-vs-restcontroller/src/main/java/lk/ijse/gdse66/WebAppConfig.java
+++ controller-vs-restcontroller/src/main/java/lk/ijse/gdse66/WebAppConfig.java
@@ -1,4 +1,24 @@
 package lk.ijse.gdse66;
 
-public class WebAppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/page/**")
+                .addResourceLocations("/WEB-INF/page/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/page/");
+        resolver.setSuffix(".html");
+        System.out.println(resolver);
+        return resolver;
+    }
 }
