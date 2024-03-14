3b1572115c4101f059d58b3f8878b1db5c3bc67f
Bethmi Jayamila <bethmij@gmail.com>
2024-03-14 09:47:04 +0530

    modify OrderController

--- mapping-spec/src/main/java/lk/ijse/gdse66/api/OrderController.java
+++ mapping-spec/src/main/java/lk/ijse/gdse66/api/OrderController.java
@@ -1,4 +1,23 @@
 package lk.ijse.gdse66.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    //    @GetMapping
+    //    public String getOrders(){
+    //        return "getOrders()";
+    //    }
+
+    @GetMapping
+    public String getOrders(@RequestHeader("X-token") String token){
+        return String.format("getOrders(String token) : %s",token);
+    }
+
+
 }
